256ffa19f062baa2b775ab61e6cc84a8f545a7bb
yourhuckleberry <githuckleberry@gmail.com>
2020-07-16 14:48:54 -0700

    Hash Passwords & Register Route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { body, validationResult, check } = require('express-validator');
 
 const User = require('../models/User');
@@ -16,13 +17,39 @@ router.post('/', [
     check('password','Please enter a password with 6 or more characters').isLength({
         min: 6
     })
-], (req,res) => {
+], async (req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
 // will display a "passed" when all validation is complete ( passwords,emails,etc)
-    res.send('passed');
-});  // "/" points to API/USERS
+
+    const { name,email,password} = req.body;
+    try {
+        let user = await User.findOne({email});
+
+        if(user){
+            return res.status(400).json({msg: 'User already exists'});
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+// encrpyt password with salt
+        const salt = await bcrypt.genSalt(10);
+// Hash the password
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User saved');
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+}); 
 
 module.exports = router;  //  "/" points to API/USERS
