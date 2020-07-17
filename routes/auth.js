bc073e1095295ab743583520f225977bfbe9b31c
yourhuckleberry <githuckleberry@gmail.com>
2020-07-17 10:41:08 -0700

    Contact Models Created: Name:,email:,phone:,type:

--- routes/auth.js
+++ routes/auth.js
@@ -68,7 +68,6 @@ async (req,res) => {
             res.json({ token });
         } )
         ;
-
     } catch (err){
         console.error(err.message);
         res.status(500).send('Server Error');
